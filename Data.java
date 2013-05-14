f12be08a8238d00a86a37e882a92b068048ca813
kranjan94 <kushal.ranjan94@gmail.com>
2013-05-14 14:55:09 -0700

    Added covariance calculation and covariance matrix construction

--- Data.java
+++ Data.java
@@ -14,6 +14,42 @@ class Data {
 		matrix = vals;
 	}
 	
+	/**
+	 * Constructs the covariance matrix for this data set.
+	 * @return	the covariance matrix of this data set
+	 */
+	double[][] covarianceMatrix() {
+		double[][] out = new double[matrix.length][matrix.length];
+		for(int i = 0; i < out.length; i++) {
+			for(int j = 0; j < out.length; j++) {
+				double[] dataA = MatrixMath.getColumn(matrix, i);
+				double[] dataB = MatrixMath.getColumn(matrix, j);
+				out[i][j] = covariance(dataA, dataB);
+			}
+		}
+		return out;
+	}
+	
+	/**
+	 * Returns the covariance of two data vectors.
+	 * @param a	double[] of data
+	 * @param b	double[] of data
+	 * @return	the covariance of a and b, cov(a,b)
+	 */
+	static double covariance(double[] a, double[] b) {
+		if(a.length != b.length) {
+			throw new MatrixException("Cannot take covariance of different dimension vectors.");
+		}
+		double divisor = a.length - 1;
+		double sum = 0;
+		double aMean = mean(a);
+		double bMean = mean(b);
+		for(int i = 0; i < a.length; i++) {
+			sum += (a[i] - aMean) * (b[i] - bMean);
+		}
+		return sum/divisor;
+	}
+	
 	
 	/**
 	 * Normalizes the data matrix so that each column is centered at 0.
@@ -31,7 +67,7 @@ class Data {
 	 * Calculates the mean of an array of doubles.
 	 * @param entries	input array of doubles
 	 */
-	double mean(double[] entries) {
+	static double mean(double[] entries) {
 		double out = 0;
 		for(double d: entries) {
 			out += d/entries.length;
